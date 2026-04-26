6998350d4d537e76beb6faeb985c89b584ac06d0
agusdwyna <dwiyanaikadek@gmail.con>
2026-04-27 05:48:40 +0800

    scroll to top added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SurfCoaching from "./SurfCoaching";
 import GuidedTrips from "./GuidedTrips";
 import Contact from "./Contact";
 import FAQ from "./FAQ";
+import ScrollToTop from "./components/ScrollToTop";
 
 
 
@@ -17,6 +18,7 @@ import FAQ from "./FAQ";
 function App() {
   return (
     <BrowserRouter>
+      {/* <ScrollToTop /> */}
       <div className="bg-[#f5f7f8] text-[#101518] min-h-screen font-['Plus_Jakarta_Sans']">
         
         <Header />
